190019ff7ee48e7ce384cb3d3129f3a170e064ac
Sahel Zaffarulla <sahel@goodrichlogistics.local>
2024-10-14 17:22:57 +0530

    changes

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 import assets from '../../../public/assets/assets';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800 text-white w-full">
       <div className='flex space-x-3 items-center'>
-        <img src={assets.glpl_logo} className='w-14' alt="" />
+        <Image src={assets.glpl_logo} width={56} height={56} alt="GLPL Logo" />
         <h1 className="text-xl font-bold">GLPL Sim Manager</h1>
       </div>
       <div className="flex items-center">
